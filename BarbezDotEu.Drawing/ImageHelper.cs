aebfb7783e042b880163d6d39585cd0973c73e2a
Hannes Barbez <hannesbarbez@hotmail.com>
2025-05-24 00:05:21 +0200

    Add more methods

--- BarbezDotEu.Drawing/ImageHelper.cs
+++ BarbezDotEu.Drawing/ImageHelper.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 
@@ -25,12 +26,12 @@ namespace BarbezDotEu.Drawing
         }
 
         /// <summary>
-        /// Converts a bitmap image to a byte array.
+        /// Converts an image to a byte array.
         /// </summary>
-        /// <param name="img">The bitmap image.</param>
+        /// <param name="img">The image.</param>
         /// <param name="format">The image format.</param>
         /// <returns>The byte array.</returns>
-        public static byte[] ImageToByte(Bitmap img, ImageFormat format)
+        public static byte[] ImageToByte(Image img, ImageFormat format)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -41,12 +42,26 @@ namespace BarbezDotEu.Drawing
         }
 
         /// <summary>
+        /// Converts an image from the specified file path to a byte array in the specified image format.
+        /// </summary>
+        /// <param name="imagePath">The file path of the source image.</param>
+        /// <param name="format">The desired image format.</param>
+        /// <returns>A byte array representing the converted image.</returns>
+        public static byte[] ConvertToImageFormat(string imagePath, ImageFormat format)
+        {
+            using (Image img = CreateImage(imagePath))
+            {
+                return ImageToByte(img, format);
+            }
+        }
+
+        /// <summary>
         /// Renders an object's visual representation to an image with a specified background color.
         /// </summary>
-        /// <param name="obj">The object to render (must implement IViewObject).</param>
+        /// <param name="object">The object to render (must implement IViewObject).</param>
         /// <param name="destination">The destination image.</param>
         /// <param name="backgroundColor">The background color to use.</param>
-        public static void GetImage(object obj, Image destination, Color backgroundColor)
+        public static void GetImage(object @object, Image destination, Color backgroundColor)
         {
             using (Graphics graphics = Graphics.FromImage(destination))
             {
@@ -63,7 +78,7 @@ namespace BarbezDotEu.Drawing
                 {
                     deviceContextHandle = graphics.GetHdc();
 
-                    IViewObject viewObject = obj as IViewObject;
+                    IViewObject viewObject = @object as IViewObject;
                     viewObject.Draw(1, -1, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, deviceContextHandle, ref rectangle, IntPtr.Zero, IntPtr.Zero, 0);
                 }
                 finally
@@ -77,36 +92,75 @@ namespace BarbezDotEu.Drawing
         }
 
         /// <summary>
-        /// Converts a bitmap image to a byte array with a watermark.
+        /// Converts a image to a byte array with a watermark.
         /// </summary>
-        /// <param name="img">The bitmap image.</param>
+        /// <param name="image">The image.</param>
         /// <param name="watermarkFilePath">The watermark file path.</param>
         /// <param name="format">The image format.</param>
         /// <returns>The byte array.</returns>
-        public static byte[] ImageToByte(Bitmap img, string watermarkFilePath, ImageFormat format)
+        public static byte[] Watermark(Image image, string watermarkFilePath, ImageFormat format)
         {
             using (Image watermark = CreateImage(watermarkFilePath))
-            using (Graphics g = Graphics.FromImage(img))
             {
-                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                return Watermark(image, format, watermark);
+            }
+        }
 
+        /// <summary>
+        /// Converts an image at the specified file path to a byte array with a watermark.
+        /// </summary>
+        /// <param name="imageFilePath">The file path of the image to watermark.</param>
+        /// <param name="watermarkFilePath">The file path of the watermark image.</param>
+        /// <param name="format">The image format for conversion.</param>
+        /// <returns>A byte array representing the watermarked image.</returns>
+        public static byte[] Watermark(string imageFilePath, string watermarkFilePath, ImageFormat format)
+        {
+            using (Image img = CreateImage(imageFilePath))
+            {
+                using (Image watermark = CreateImage(watermarkFilePath))
+                {
+                    return Watermark(img, format, watermark);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts the given image to a byte array with a watermark applied.
+        /// </summary>
+        /// <param name="image">The original image.</param>
+        /// <param name="format">The image format for conversion.</param>
+        /// <param name="watermark">The watermark image to apply.</param>
+        /// <returns>A byte array representing the watermarked image.</returns>
+        public static byte[] Watermark(Image image, ImageFormat format, Image watermark)
+        {
+            using (Graphics graphics = Graphics.FromImage(image))
+            {
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
                 int repeatX = 1;
                 int repeatY = 1;
 
-                if (watermark.Width > 0) repeatX = 2 + img.Width / watermark.Width;
-                if (watermark.Height > 0) repeatY = 2 + img.Height / watermark.Height;
-
+                if (watermark.Width > 0) repeatX = 2 + image.Width / watermark.Width;
+                if (watermark.Height > 0) repeatY = 2 + image.Height / watermark.Height;
                 for (int x = 0; x < repeatX; x++)
-                    for (int y = 0; y < repeatY; y++)
-                        g.DrawImage(watermark, new Point(img.Width - x * watermark.Width, img.Height - y * watermark.Height));
-
-                using (MemoryStream ms = new MemoryStream())
                 {
-                    img.Save(ms, format);
-                    img.Dispose();
-                    g.Flush();
-                    return ms.ToArray();
+                    for (int y = 0; y < repeatY; y++)
+                    {
+                        graphics.DrawImage(watermark, new Point(image.Width - x * watermark.Width, image.Height - y * watermark.Height));
+                    }
                 }
+
+                return SaveImage(image, format, graphics);
+            }
+        }
+
+        private static byte[] SaveImage(Image image, ImageFormat format, Graphics graphics)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                image.Dispose();
+                graphics.Flush();
+                return ms.ToArray();
             }
         }
     }
